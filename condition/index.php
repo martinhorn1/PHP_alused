89712dc7bed57f69764a30f0548736386101d39e
martinhorniktkhk <martin.horn@khk.ee>
2018-12-13 16:18:28 +0200

    Switch case

--- condition/index.php
+++ condition/index.php
@@ -6,16 +6,23 @@
 // tegevused, kui tingimus ei kehti
 // }
 
-$arv = rand(0,100);
-
 echo '<h4>Tingimuslaused</h4>';
-if ($arv  >= 0 and $arv < 25) {
-    echo '<p style="color: green">'.$arv.'</p>';
-} else if ($arv  >= 25 and $arv < 50){
-    echo '<p style="color: red">'.$arv.'</p>';
-} else if ($arv  >= 50 and $arv < 75){
-    echo '<p style="color: blue">'.$arv.'</p>';
-} else if ($arv  >= 75 and $arv < 100){
-    echo '<p style="color: orange">'.$arv.'</p>';
+
+$foor = 'sinine';
+
+switch ($foor) {
+    case 'punane':
+        echo '<p style="color: red">Punane</p>';
+        break;
+    case 'kollane':
+        echo '<p style="color: yellow">Kollane</p>';
+        break;
+    case 'roheline':
+        echo '<p style="color: green">Roheline</p>';
+        break;
+    default:
+        echo '<p>Mingi teine</p>';
+        break;
 }
+
     ?>
