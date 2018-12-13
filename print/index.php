93b8685eeace382f5955707da71f212c4619393c
martinhorniktkhk <martin.horn@khk.ee>
2018-12-13 15:04:42 +0200

    Väljastus print ja echo

---
+++ print/index.php
@@ -0,0 +1,9 @@
+<?php
+// Teksti väljastus print käsuga
+print 'See on esimene lause PHP keeles'.'<br>';
+print 'See on teine lause PHP keeles'.'<br>';
+
+// Teksti väljastus echo käsuga
+echo 'See tekst on väljastatud echo-käsuga'.'<br>';
+
+?>
