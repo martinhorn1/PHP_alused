0f3e8f4ed2ca09399af75caba6f492745a73cdca
martinhorniktkhk <martin.horn@khk.ee>
2018-12-13 16:12:08 +0200

    If-else 2

--- condition/index.php
+++ condition/index.php
@@ -9,9 +9,13 @@
 $arv = rand(0,100);
 
 echo '<h4>Tingimuslaused</h4>';
-if ($arv % 2 == 0) {
+if ($arv  >= 0 and $arv < 25) {
     echo '<p style="color: green">'.$arv.'</p>';
-} else {
+} else if ($arv  >= 25 and $arv < 50){
     echo '<p style="color: red">'.$arv.'</p>';
+} else if ($arv  >= 50 and $arv < 75){
+    echo '<p style="color: blue">'.$arv.'</p>';
+} else if ($arv  >= 75 and $arv < 100){
+    echo '<p style="color: orange">'.$arv.'</p>';
 }
-?>
+    ?>
