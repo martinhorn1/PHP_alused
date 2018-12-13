b3de31d9a54af264bcfb8e6607e81b98d887bd78
martinhorniktkhk <martin.horn@khk.ee>
2018-12-13 16:41:04 +0200

    For tsükkel - värvitabel

--- cycle/index.php
+++ cycle/index.php
@@ -1,20 +1,18 @@
 <?php
-
+header('Refresh: 1');
 // for
 /*
  * for ($jm = alg; $jm < lopp; $jm++) {
  *  tegevused, mis sooritatakse nii kaua, kuni tsükli $jm väärtus on väiksem kui lõppväärtus*/
 
 echo '<h4>Tsüklid - for</h4>';
-
-for ($arv = 1; $arv <= 10; $arv++) {
-    if ($arv % 2 == 0) {
-        $varv = 'green';
-    }
-    else {
-        $varv = 'red';
-    }
-    echo '<div style="color: '.$varv.'">'.$arv.'</div>';
+echo '<table>';
+for ($rida = 1; $rida <= 5; $rida++) {
+    $varv = '#'.rand(0,9).rand(0,9).rand(0,9);
+    echo '<tr>';
+        echo '<td style="background: '.$varv.'">';
+        echo '&nbsp';
+        echo '</td>';
+    echo '<tr>';
 }
-
 ?>
