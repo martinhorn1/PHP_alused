dad23cddbade3cd0e7c1aaf8fde186f716e2d750
martinhorniktkhk <martin.horn@khk.ee>
2018-12-14 10:27:31 +0200

    For tsükkel - korrutustabel

--- cycle/index.php
+++ cycle/index.php
@@ -1,18 +1,17 @@
 <?php
-header('Refresh: 1');
 // for
 /*
  * for ($jm = alg; $jm < lopp; $jm++) {
- *  tegevused, mis sooritatakse nii kaua, kuni tsükli $jm väärtus on väiksem kui lõppväärtus*/
+ *  tegevused, mis sooritatakse nii kaua, kuni tsükli $jm väärtus
+ * on väiksem kui lõppväärtus*/
 
 echo '<h4>Tsüklid - for</h4>';
 echo '<table>';
-for ($rida = 1; $rida <= 5; $rida++) {
-    $varv = '#'.rand(0,9).rand(0,9).rand(0,9);
+for ($rida = 1; $rida <= 10; $rida++) {
     echo '<tr>';
-        for ($veerg = 1; $veerg <=5; $veerg++) {
-            echo '<td style="background: '.$varv.'">';
-            echo '&nbsp';
+        for ($veerg = 1; $veerg <=10; $veerg++) {
+            echo '<td>';
+            echo $rida*$veerg.'&nbsp';
             echo '</td>';
         }
     echo '<tr>';
