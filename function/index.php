6040be78e43bb06a8198588d142d5c333c70848b
martinhorniktkhk <martin.horn@khk.ee>
2018-12-17 13:49:22 +0200

    Funktsioon - seisundi meelespidamine

--- function/index.php
+++ function/index.php
@@ -48,6 +48,17 @@ function meaningOfLife() {
 }
 meaningOfLife();
 
+echo 'Funktsiooni seisundi meelespidamine<br>';
+$num_of_calls = 0;
+function andAnotherThing($txt) {
+    global $num_of_calls;
+    $num_of_calls++;
+    print '<h1>'.$num_of_calls.'. '.$txt.'</h1>';
+}
+andAnotherThing('Widgets');
+print ('We build a fine range of widgets');
+andAnotherThing('Doodads');
+print ('Finest in the world');
 
 
 ?>
