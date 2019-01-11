2f628d872804928a8d0b62de0f06789363e7fb93
Martin <martin.horn@khk.ee>
2019-01-12 01:13:14 +0200

    Massiivi defineerimine array() funktsiooni abil

--- array/index.php
+++ array/index.php
@@ -1,39 +1,6 @@
 <?php
 
-$kasutajad = array(
-    array(
-        'alice',
-        'Alice',
-        'Liddell',
-        'female'
-    ),
-    array(
-        'bob',
-        'Bob',
-        'Builder',
-        'male'
-    )
-);
+$users = array('Bert', 'Sharon', 'Betty', 'Harry');
 
-for ($i = 0; $i < count($kasutajad); $i++) {
-    if ($kasutajad[$i][3] == 'female') {
-        echo '<div style="color: blue">';
-    } else {
-        echo '<div style="color: red">';
-    }
-    foreach ($kasutajad[$i] as $info) {
-        echo $info.'<br>';
-    }
-    echo '<hr>';
-}
-
-echo '<table>';
-for ($i = 0; $i < $kasutajad; $i++) {
-    echo '<tr>';
-    foreach ($kasutajad[$i] as $info) {
-        echo '<td>'.$info.'</td>';
-    }
-    echo '</tr>';
-}
-echo '</table>';
+print $users[2];
 ?>
