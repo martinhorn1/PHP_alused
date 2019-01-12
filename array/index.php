d15e0feb059f1b2066f8a96fd07592a0e22af3c5
Martin <martin.horn@khk.ee>
2019-01-12 15:57:47 +0200

    Mitmemõõtmelise massiivi väljastamine

--- array/index.php
+++ array/index.php
@@ -1,14 +1,17 @@
 <html>
 <head>
-    <title>Listing 7.2</title>
+    <title>Listing 7.3</title>
 </head>
 <body>
 <?php
 
-$character = array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=>30, 'special power'=> 'x-ray vision');
+$character = array(array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=>30, 'special power'=> 'x-ray vision'), array('name'=> 'Sally', 'occupation'=> 'superhero', 'age'=>24, 'special power'=> 'superhuman strength'), array('name'=> 'Mary', 'occupation'=> 'arch villain', 'age'=>63, 'special power'=> 'nanotechnology'));
 
-foreach ($character as $key=>$val) {
-    print $key = $val.'<br>';
+foreach ($character as $val) {
+    foreach ( $val as $key=>$final_val) {
+        print $key.': '.$final_val.'<br>';
+    }
+    print '<br>';
 }
 ?>
 </body>
