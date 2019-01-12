869053365c5607c4c9fa9a05938d853e935f0ff8
Martin <martin.horn@khk.ee>
2019-01-12 15:28:49 +0200

    Massiivi suuruse kätte saamine, viimase elemendi väljastamine

--- array/index.php
+++ array/index.php
@@ -1,14 +1,8 @@
-<html>
-<head>
-    <title>Listing 7.1</title>
-</head>
-<body>
 <?php
 
-$characters = array(array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=> '30', "special power"=> 'x-ray vision'), array( 'name'=> 'Sally', 'occupation'=> 'superhero', 'age'=> 24, 'specialty'=> 'superhuman strength'), array('name'=> 'Mary', 'occupation'=> 'arch villain', 'age'=> 63, 'specialty'=> 'nanotechnology'));
+$users = array('Bert', 'Sharon', 'Betty', 'Harry');
 
 
-print $characters[0]['occupation'];
+print $users[count($users)-1];
 ?>
-</body>
-</html>
+
