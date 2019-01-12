a9a781f112cf15efac407da170c0c9eae965502c
Martin <martin.horn@khk.ee>
2019-01-12 13:48:15 +0200

    Mitmemõõtmelised massiivid

--- array/index.php
+++ array/index.php
@@ -1,7 +1,14 @@
+<html>
+<head>
+    <title>Listing 7.1</title>
+</head>
+<body>
 <?php
 
-$character = array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=> '30', "special power"=> 'x-ray vision');
+$characters = array(array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=> '30', "special power"=> 'x-ray vision'), array( 'name'=> 'Sally', 'occupation'=> 'superhero', 'age'=> 24, 'specialty'=> 'superhuman strength'), array('name'=> 'Mary', 'occupation'=> 'arch villain', 'age'=> 63, 'specialty'=> 'nanotechnology'));
 
 
-print $character['age'];
+print $characters[0]['occupation'];
 ?>
+</body>
+</html>
