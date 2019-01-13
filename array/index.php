8943c66361726880e43f4247a83e52d9c3e4b5fc
Martin <martin.horn@khk.ee>
2019-01-13 17:46:15 +0200

    Sidestatud massiivi sorteerimine ksort() funktsiooni abil

--- array/index.php
+++ array/index.php
@@ -1,6 +1,6 @@
 <?php
-$first = array('first'=>5, 'second'=>2, 'third'=>1);
-asort($first);
+$first = array('x'=>5, 'a'=>2, 'f'=>1);
+ksort($first);
 foreach ($first as $key => $var) {
     print $key = $var.'<br>';
 }
