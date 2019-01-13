542ee170a63a43d2661495fdc44c2db83d4dadf6
Martin <martin.horn@khk.ee>
2019-01-13 18:42:24 +0200

    Erinevate kehade jaoks ruumala leidmine

--- form/arvutus.php
+++ form/arvutus.php
@@ -1,4 +1,14 @@
 <?php
 $raadius = $_GET['rad'];
-echo 'Kera ruumala on '.(4/3*3.14*$raadius**3).' cm^3';
+$keha = $_GET['kuju'];
+$kõrgus = $_GET['korg'];
+if ($keha == 1) {
+    echo 'Kera ruumala on '.(4/3*3.14*$raadius**3).' cm^3';
+}
+else if ($keha == 2) {
+    echo 'Silindri ruumala on '.($kõrgus*3.14*$raadius**2).' cm^3';
+}
+else if ($keha == 3) {
+    echo 'Koonuse ruumala on '.($kõrgus/3*3.14*$raadius**2).' cm^3';
+}
 ?>
