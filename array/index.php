9cad87d263ca5836e1475db2cb6f77523e6f94a4
Martin <martin.horn@khk.ee>
2019-01-13 12:42:07 +0200

    Massiivide ühendamine array_merge() funktsiooni abil

--- array/index.php
+++ array/index.php
@@ -1,19 +1,8 @@
-<html>
-<head>
-    <title>Listing 7.3</title>
-</head>
-<body>
 <?php
-
-$character = array(array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=>30, 'special power'=> 'x-ray vision'), array('name'=> 'Sally', 'occupation'=> 'superhero', 'age'=>24, 'special power'=> 'superhuman strength'), array('name'=> 'Mary', 'occupation'=> 'arch villain', 'age'=>63, 'special power'=> 'nanotechnology'));
-
-foreach ($character as $val) {
-    foreach ( $val as $key=>$final_val) {
-        print $key.': '.$final_val.'<br>';
-    }
-    print '<br>';
+$first = array('a', 'b', 'c');
+$second = array(1,2,3);
+$third = array_merge ($first, $second);
+foreach ($third as $val) {
+    print $val.'<br>';
 }
 ?>
-</body>
-
-</html>
