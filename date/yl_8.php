f41886d15f87d905b2588b330a11bf433d8be684
martinhorniktkhk <martin.horn@khk.ee>
2019-01-14 10:59:49 +0200

    Nädalapäev eesti keeles

--- date/yl_8.php
+++ date/yl_8.php
@@ -1,4 +1,32 @@
 <?php
-date_default_timezone_set('Europe/Tallinn');
-echo date('d.m.Y G:i');
+$paevad = array(1=>'esmaspäev', 'teisipäev', 'kolmapäev', 'neljapäev', 'reede', 'laupäev', 'pühapäev');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = date('n');
+$aasta = date('Y');
+
+$paevanimi = date('l');
+if ($paevanimi == 'Monday') {
+    echo 'Esmaspäev';
+}
+else if ($paevanimi == 'Tuesday') {
+    echo 'Teisipäev';
+}
+else if ($paevanimi == 'Wednesday') {
+    echo 'Kolmapäev';
+}
+else if ($paevanimi == 'Thursday') {
+    echo 'Neljapäev';
+}
+else if ($paevanimi == 'Friday') {
+    echo 'Reede';
+}
+else if ($paevanimi == 'Saturday') {
+    echo 'Laupäev';
+}
+else if ($paevanimi == 'Sunday') {
+    echo 'Pühapäev';
+}
 ?>
+
+
