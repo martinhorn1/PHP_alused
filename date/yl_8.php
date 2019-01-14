e6246f9a9a34f7a0c81d4acc43de182198e38bd7
Martin <martin.horn@khk.ee>
2019-01-14 22:15:16 +0200

    Kuupäevade võrdlus

--- date/yl_8.php
+++ date/yl_8.php
@@ -1,8 +1,14 @@
 <?php
 
-$jaan = mktime(0,0,0,6,24,2019);
+$synd1 = date('Y', mktime(0,0,0,11,6,1980));
+$synd2 = date('Y', mktime(0,0,0,2,20,1993));
 
-echo 'Jaanipäevani on '.round(date($jaan-strtotime('now'))/3600/24).' päeva.';
+if ($synd1 > $synd2) {
+    echo 'Noorem '.($synd1-$synd2).' aastat';
+}
+else {
+    echo 'Vanem '.($synd2-$synd1).' aastat';
+}
 
 ?>
 
