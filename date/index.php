995ba95d04c308ebc38d3c45b4420572e74a96d7
martinhorniktkhk <martin.horn@khk.ee>
2019-01-14 10:24:44 +0200

    Pika kuupäeva eestistamine

--- date/index.php
+++ date/index.php
@@ -1,4 +1,10 @@
 <?php
-date_default_timezone_set('Europe/Tallinn');
-echo date('d.m.Y G:i');
+//kuude massiiv
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;	//22.veebruar2013
 ?>
