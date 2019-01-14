00454246ff62127e87b7621624fe5ad42c094e9b
Martin <martin.horn@khk.ee>
2019-01-14 22:02:02 +0200

    Päevi jaanipäevani

--- date/yl_8.php
+++ date/yl_8.php
@@ -1,33 +1,9 @@
 <?php
-//kuude massiiv
-$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
-//kuupäevad massiividesse
-$paev = date('d');
-$kuu = $eesti_kuud[date('n')];
-$aasta = date('Y');
 
-$paevanimi = date('l');
-if ($paevanimi == 'Monday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' esmaspäev';
-}
-else if ($paevanimi == 'Tuesday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' teisipäev';
-}
-else if ($paevanimi == 'Wednesday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' kolmapäev';
-}
-else if ($paevanimi == 'Thursday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' neljapäev';
-}
-else if ($paevanimi == 'Friday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' reede';
-}
-else if ($paevanimi == 'Saturday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' laupäev';
-}
-else if ($paevanimi == 'Sunday') {
-    echo $paev.'.'.$kuu.' '.$aasta.' pühapäev';
-}
+$jaan = mktime(0,0,0,6,24,2019);
+
+echo 'Jaanipäevani on '.round(date($jaan-strtotime('now'))/3600/24).' päeva.';
+
 ?>
 
 
