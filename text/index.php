8d2141afbca7843886a311b3fd803a7be4ccde20
Martin <martin.horn@khk.ee>
2019-01-15 20:35:50 +0200

    Teksti kärpimine

--- text/index.php
+++ text/index.php
@@ -1,6 +1,7 @@
 <?php
-$tekst = 'Experience is the teacher of fools';
-echo strlen($tekst);			//34
-echo '<br>';
-echo str_word_count($tekst);	//6
+$tekst = ' 	A woman should soften but not weaken a man   ';
+echo "<pre>$tekst</pre>";
+echo "<pre>".trim($tekst)."</pre>";
+echo "<pre>".ltrim($tekst)."</pre>";
+echo "<pre>".rtrim($tekst)."</pre>";
 ?>
