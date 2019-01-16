143c0dfe1e750ee8fee4c5b59149b44fcbfde238
Martin <martin.horn@khk.ee>
2019-01-16 22:11:53 +0200

    Iga tähe järele punkti lisamine

--- text/index.php
+++ text/index.php
@@ -1,4 +1,7 @@
 <?php
-$nimi = 'mArIo';
-echo 'Tere, '.ucfirst(strtolower($nimi)).'!';
+$sisend = 'stalker';
+for ($i = 0; $i < strlen($sisend); $i++) {
+    echo strtoupper($sisend[$i]);
+    echo '.';
+}
 ?>
