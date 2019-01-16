b1f37215d951bc21e638f04a1e5545ff744e0023
Martin <martin.horn@khk.ee>
2019-01-16 22:05:43 +0200

    Nime vormindamine

--- text/index.php
+++ text/index.php
@@ -1,7 +1,4 @@
 <?php
-$tekst = 'Pai papa, pane paadile punased purjed peale';
-$asendus = 'emme';
-$otsitav_algus = 4;
-$otsitav_pikkus = 4;
-echo substr_replace($tekst, $asendus, $otsitav_algus, $otsitav_pikkus);
+$nimi = 'mArIo';
+echo 'Tere, '.ucfirst(strtolower($nimi)).'!';
 ?>
