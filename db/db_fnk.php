059604810a13147a41c723669dbd39ba4b6433ff
Martin <martin.horn@khk.ee>
2019-01-27 13:29:20 +0200

    Ühenduse loomine andmebaasi serveriga

---
+++ db/db_fnk.php
@@ -0,0 +1,12 @@
+<?php
+function connect_db($hostName, $dbUser, $dbPass, $dbName) {
+    $conn = mysqli_connect($hostName, $dbUser, $dbPass, $dbName);
+
+    if (!$conn) {
+        echo 'unable to connect to MySQL<br>';
+        echo mysqli_connect_error().'<br>';
+        exit;
+    }
+    return $conn;
+}
+?>
