50de714dbe5dd0b7d75fd55203f3dc79eef53969
martinhorniktkhk <martin.horn@khk.ee>
2019-01-28 13:12:37 +0200

    Loodud kasutaja sessiooni lõpetamine

--- session/index.php
+++ session/index.php
@@ -4,6 +4,7 @@ require_once 'fnk.php';
 
 if(isset($_SESSION['user'])){
     echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<a href="acts/logout.php">Logi välja</a>';
 } else {
     $loginForm = htmlFromFile('login');
     echo $loginForm;
